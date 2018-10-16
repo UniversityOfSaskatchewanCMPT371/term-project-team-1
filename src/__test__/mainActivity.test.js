45e88e7641a1254443b18a3f623a61b68f407e28
Jude Pineda <jay.p@usask.ca>
2018-10-16 14:03:17 -0600

    Made a placeholder mainActivity.js file that Jesse's mainActivity.test.js file can point to

--- src/__test__/mainActivity.test.js
+++ src/__test__/mainActivity.test.js
@@ -14,3 +14,10 @@
 //   })
 
 // })
+
+// temporary placeholder while we wait for molham's code
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
