a21676644506d7d7df628420ce88979631289016
Jude Pineda <jay.p@usask.ca>
2018-10-19 15:21:08 -0600

    Included Jesse's problematic files

--- src/App.js
+++ src/App.js
@@ -1,56 +1,39 @@
-import { Navigation } from 'react-native-navigation';
-import { Provider } from 'react-redux';
-import { Screens, registerScreens } from './components/Navigation';
-import homeIcon from './assets/ic_home/ic_home.png';
-import profileIcon from './assets/ic_settings/ic_settings.png';
-import strings from './localization';
-import Colors from './helpers/Colors';
-import { store, persist } from './reducers';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-class App {
-  constructor(rootStore, provider) {
-    this.store = rootStore;
-    this.provider = provider;
+ import React from 'react';
+ import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+ export default class App extends React.Component {
+
+      constructor(props) {
+          super(props);
+      }
+
+    sum(a,b){
+        const sumVal = a+b;
+        return sumVal;
+      }
+
+      render() {
+ 		return (
+              <View style={styles.container}>
+                  <Text>{'sum of 3 and 2 = ' + this.sum(3,2)}</Text>
+              </View>
+          );
+      }
   }
 
-  startLoggedInApp = () => {
-    Navigation.startTabBasedApp({
-      tabs: [
-        {
-          screen: Screens.Home,
-          icon: homeIcon,
-          label: strings.home,
-        },
-        {
-          screen: Screens.Profile,
-          icon: profileIcon,
-          label: strings.profile,
-        },
-      ],
-      tabsStyle: {
-        tabBarSelectedButtonColor: Colors.primary,
-        tabBarButtonColor: Colors.gray,
-        initialTabIndex: 0,
+  const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'center',
       },
-      animationType: 'fade',
-    });
-  }
-
-  startLoggedOutApp = () => {
-    Navigation.startSingleScreenApp({
-      screen: {
-        screen: Screens.Login,
-      },
-      animationType: 'fade',
-    });
-  }
-
-  startApp = () => {
-    registerScreens(this.store, this.provider);
-    persist(() => {
-      this.startLoggedOutApp();
-    });
-  }
-}
-
-export default new App(store, Provider);
+  });
---
+++ src/__test__/App.test.js
@@ -0,0 +1,15 @@
+const sum = require('./sum');
+import React from 'react';
+import App from '../';
+
+import renderer from 'react-test-renderer';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<App/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('adds 3+2 to equal 5', () => {
+ expect(sum(3,2)).toBe(5);
+ expect(sum(3,2)).toNotBe(4);
+});
