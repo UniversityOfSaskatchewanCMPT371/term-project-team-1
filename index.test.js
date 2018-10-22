bcd8412d1d58b0022af066ae343cf2ea6ea74217
Jesse <jrr867@mail.usask.ca>
2018-10-21 20:35:48 -0600

    Added a couple more files in package.json, set up further tests/commented code that may help for testing buttons and testing rendering the starting index component for ID3.

--- index.test.js
+++ index.test.js
@@ -1,14 +1,14 @@
-import Button from '../Button';
+import App from './index';
 import { shallow,configure } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
-//Imports to get Enzyme to work if possible -> will work on later
 
 
 test('renders correctly', () => {
-  const wrapper = shallow(<Button text="Test"/>);
-  expect(wrapper).toMatchSnapshot();
+    //Add Rendering for the starting piece.
+    //This is different because this is of type 'App' which isn't a component, which means enzyme doesn't work.
+    //This will need some other way to test it.
 });
--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,5 +1,5 @@
 import Header from '../Header';
-import { shallow,configure } from 'enzyme';
+import { shallow,configure,mount } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
@@ -13,3 +13,17 @@ test('renders correctly', () => {
   expect(wrapper).toMatchSnapshot();
   //expect(tree).toMatchSnapshot();
 });
+
+/*this test currently does not work; note that this is due to requiring an actual DOM document to work from.
+this shouldn't be difficult, but there's a lot of people saying that shallow renders are enough to make sure it renders properly
+at least on the face of it. I will go into testing SPECIFIC pieces of components rendering properly for ID3 now that
+I can actually write test code that actually tests things. */
+
+/*it('Formats Title Correctly', () => {
+  const wrapper = mount(
+    <Header headerTitle="This is a title"/>
+  );
+  const text = wrapper.find("Text").text();
+
+  expect(text).toEqual("This is a title");
+});*/
