ef96ae7269333964e5638419daeece117df0d7d6
Jude Pineda <jay.p@usask.ca>
2018-10-23 15:57:09 -0600

    Commited some changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-
 function sum(a, b){
   const sumVal = a + b;
   return sumVal;
-}
-module.exports = sum;
+} module.exports = sum;
+
 
 export default class App extends Component {
 
   // constructor(props) {
   //   super(props);
   // }
-  
+
   render() {
  	  return (
       <View style={styles.container}>
