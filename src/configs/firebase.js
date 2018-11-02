2cab90aa8d6a285e5975ee43bff66c6ace831ec9
Jude Pineda <jay.p@usask.ca>
2018-11-01 19:45:20 -0600

    Added Firebase files for Android

---
+++ src/configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyByzuFR21QSm1c7nj6JSNpdRRqMzkQsI1c",
+  authDomain: "healthdeck-43498.firebaseapp.com",
+  databaseURL: "https://healthdeck-43498.firebaseio.com",
+  projectId: "healthdeck-43498",
+  storageBucket: "healthdeck-43498.appspot.com",
+  messagingSenderId: "610614263722"
+}
