72303471cf04cbc14cce0849fd45c7d073f33063
MOLHAMKAADAN\Molham <molham_kaadan@hotmail.com>
2018-11-07 22:46:49 -0600

    Attempt to merge master with latest branch

--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-//import react from "react";
-//import Enzyme, { shallow, render, mount } from "enzyme";
-//import Adapter from "enzyme-adapter-react-16";
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
